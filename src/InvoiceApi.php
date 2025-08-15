4ee11ac60d32bf89aa72524ec1bc54c126f51ec2
Tri Nguyen Duc (Bee Tech - PHP) <tri.nguyen.duc@beetechjsc.com>
2025-08-15 13:54:51 +0700

    "Add `searchInvoice` method to `InvoiceApi` for invoice search with validation and pagination. Update `InvoiceTest` to include test ensuring response is an array."

--- src/InvoiceApi.php
+++ src/InvoiceApi.php
@@ -3,6 +3,7 @@
 namespace BeetechAsia\GoShip;
 
 use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Validator;
 
 trait InvoiceApi
 {
@@ -12,6 +13,25 @@ trait InvoiceApi
     public function getInvoices(?int $perPage = null, string $pageName = 'page', ?int $page = null): array
     {
         $query = $this->getPaginateQuery($perPage, $pageName, $page);
+
+        return $this->getRequest()->get('api/v2/invoices', $query)->json('data');
+    }
+
+    /**
+     * @throws ConnectionException
+     */
+    public function searchInvoice(?string $code = null, ?string $from = null, ?string $to = null, ?int $perPage = null, string $pageName = 'page', ?int $page = null): array
+    {
+        $search = Validator::validate(compact('code', 'from', 'to'), [
+            'code' => ['nullable', 'string'],
+            'from' => ['nullable', 'required_without:code', 'string', 'date_format:Y-m-d', 'before:to'],
+            'to' => ['nullable', 'required_without:code', 'string', 'date_format:Y-m-d'],
+        ]);
+
+        $query = array_merge(
+            $search,
+            $this->getPaginateQuery($perPage, $pageName, $page)
+        );
 
         return $this->getRequest()->get('api/v2/invoices', $query)->json('data');
     }
--- tests/InvoiceTest.php
+++ tests/InvoiceTest.php
@@ -16,3 +16,22 @@ it(
             ->toBeArray();
     }
 );
+
+it(
+    'searchInvoice must be array',
+    /**
+     * @throws ConnectionException
+     */
+    function () {
+        $invoices = fake()->boolean()
+            ? GoShip::searchInvoice('FA13HEUD')
+            : GoShip::searchInvoice(
+                from: now()->subDays(7)->format('Y-m-d'),
+                to: now()->format('Y-m-d')
+            );
+
+        expect($invoices)
+            ->dump()
+            ->toBeArray();
+    }
+);
